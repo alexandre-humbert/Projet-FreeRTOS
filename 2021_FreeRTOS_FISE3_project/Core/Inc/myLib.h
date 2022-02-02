199cdc0c767271f15d3773406af9e0d38741224e
julienchavas <75778350+julienchavas@users.noreply.github.com>
2022-02-02 12:22:38 +0100

    Mise à jour Prof

--- 2021_FreeRTOS_FISE3_project/Core/Inc/myLib.h
+++ 2021_FreeRTOS_FISE3_project/Core/Inc/myLib.h
@@ -9,13 +9,24 @@
 #define INC_MYLIB_H_
 
 void I2C_Scan(I2C_HandleTypeDef*,char*);
-void Init_MPU9250(I2C_HandleTypeDef*);
+void Init(I2C_HandleTypeDef*);
 void Init_BMP280(I2C_HandleTypeDef*);
-void Measure_T(I2C_HandleTypeDef*,double*);
-void Measure_T_BMP280(I2C_HandleTypeDef*,double*,long signed int*);
-uint8_t Measure_P(I2C_HandleTypeDef*,double*,long signed int*);
-void Measure_A(I2C_HandleTypeDef*,double*);
-void Measure_G(I2C_HandleTypeDef*,double*,double*);
-void Measure_M(I2C_HandleTypeDef*,double*,double*,double*);
+void MeasureT(I2C_HandleTypeDef*,double*);
+void MeasureT_BMP280(I2C_HandleTypeDef*,double*,long signed int*);
+uint8_t MeasureP(I2C_HandleTypeDef*,double*,long signed int*);
+void MeasureA(I2C_HandleTypeDef*,double*);
+void MeasureG(I2C_HandleTypeDef*,double*);
+void MeasureM(I2C_HandleTypeDef*,double*,double*,double*);
+void NoiseG(I2C_HandleTypeDef*,double*);
+void Average(I2C_HandleTypeDef*,double*);
+void CalibG(I2C_HandleTypeDef*,double*);
+void CalibM(I2C_HandleTypeDef*,double*,double*);
+void Roll(I2C_HandleTypeDef*,double*);
+void Pitch(I2C_HandleTypeDef*,double*);
+void Yaw(I2C_HandleTypeDef*,double*,double*);
+void Yaw2(I2C_HandleTypeDef*,double*,double*);
+void ComplementaryFilter(I2C_HandleTypeDef*,double*,double*);
+void GUI(double,double,double,double,double);
+void GUI2(double);
 
 #endif /* INC_MYLIB_H_ */
